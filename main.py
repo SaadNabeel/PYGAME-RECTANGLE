82cb97a081db4d36dd6a025146fca1f568647d95
SaadNabeel <saadnabeel2010@gmail.com>
2024-11-24 09:54:50 +0500

    update

--- main.py
+++ main.py
@@ -2,15 +2,23 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode((700, 600))
+window = pygame.display.set_mode((700, 600))
 
 done = False
+
+GREEN = (0, 255, 0)
 
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
 
-    pygame.draw.rect(screen, (0,125, 255), pygame.Rect(30, 30, 60, 60))
+    
+    window.fill((0, 0, 0))
+
+    
+    pygame.draw.rect(window, (0, 125, 255), pygame.Rect(30, 30, 60, 60))
+    pygame.draw.circle(window, GREEN, (300, 300), 50)
+    pygame.draw.circle(window, GREEN, (100, 100), 50, 3)
 
     pygame.display.flip()
